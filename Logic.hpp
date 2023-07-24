bb09751470c0b7429a921a4fd58d4f35704d96c8
andreamanini98 <andrea3.manini@mail.polimi.it>
2023-07-24 20:09:46 +0200

    Removed unused code

--- Logic.hpp
+++ Logic.hpp
@@ -3,14 +3,6 @@
 
 #define CAM_HEIGHT 1.0f
 
-// Struct containing the parameters to be passed from VTemplate.cpp
-struct LookAtStuff {
-    float deltaT;
-    float cameraYaw;
-    float cameraPitch;
-    float cameraRoll;
-};
-
 inline float normalizeAngle(float angle) {
     // Normalize the angle to be between 0 and 2*pi (0 and 360 degrees)
     angle = fmod(angle, 2 * M_PI);
@@ -41,11 +33,6 @@ void getLookAt(float Ar, glm::mat4 &ViewPrj, glm::mat4 &World, float deltaT, glm
     const float farPlane = 50.f;
     // Camera target height and distance
     const glm::vec3 upVector(0.0, 1.0, 0.0);
-
-    // Used for damping
-    //static glm::vec3 PosOld = characterPos;
-    //glm::vec3 P = PosOld * std::exp(-10 * deltaT) + characterPos * (1 - std::exp(-10 * deltaT));
-    //PosOld = P;
 
     //calculate look at matrix ( same as glm::lookAt )
     World = glm::translate(glm::mat4(1.0), characterPos) * glm::rotate(glm::mat4(1.0), characterAngle, glm::vec3(0, 1, 0));
