6ec9bf7f98e596269ca1e7cfb49843c5721d6f73
Matteo Carrara <matte.carra@gmail.com>
2023-07-25 21:49:06 +0200

    double buffering

--- Starter.hpp
+++ Starter.hpp
@@ -986,11 +986,11 @@ protected:
 
 	VkPresentModeKHR chooseSwapPresentMode(
 			const std::vector<VkPresentModeKHR>& availablePresentModes) {
-		for (const auto& availablePresentMode : availablePresentModes) {
+		/*for (const auto& availablePresentMode : availablePresentModes) {
 			if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
 				return availablePresentMode;
 			}
-		}
+		}*/
 		return VK_PRESENT_MODE_FIFO_KHR;
 	}
 
